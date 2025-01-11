434a2fe385870ad312ac162dd3022f0bc27a3312
Zhen XIN <lone.2004@gmail.com>
2025-01-12 01:50:31 +0800

    Chart added to mainwin

--- mainwin.pas
+++ mainwin.pas
@@ -5,17 +5,25 @@ unit mainwin;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls, TAGraph, TASeries, ucpuinfo;
 
 type
-
   { TMainWindow }
 
   TMainWindow = class(TForm)
     SampleBtn: TButton;
+    Chart: TChart;
+    FreqSeries: TLineSeries;
+    UsageSeries: TBarSeries;
+    UpdateTimer: TTimer;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
     procedure SampleBtnClick(Sender: TObject);
+    procedure UpdateTimerTimer(Sender: TObject);
   private
-
+    FCPUInfo: TCPUInfo;
+    FTimePoint: Integer;
   public
 
   end;
@@ -29,9 +37,43 @@ implementation
 
 { TMainWindow }
 
+procedure TMainWindow.FormCreate(Sender: TObject);
+begin
+  FCPUInfo := TCPUInfo.Create;
+  FTimePoint := 0;
+  
+  Chart.LeftAxis.Title.Caption := 'CPU Frequency (MHz)';
+  Chart.LeftAxis2.Title.Caption := 'CPU Usage (%)';
+  Chart.BottomAxis.Title.Caption := 'Time';
+  
+  UpdateTimer.Interval := 1000;
+  UpdateTimer.Enabled := True;
+end;
+
+procedure TMainWindow.FormDestroy(Sender: TObject);
+begin
+  FCPUInfo.Free;
+end;
+
 procedure TMainWindow.SampleBtnClick(Sender: TObject);
 begin
 
+end;
+
+procedure TMainWindow.UpdateTimerTimer(Sender: TObject);
+begin
+  Inc(FTimePoint);
+  
+  FreqSeries.AddXY(FTimePoint, FCPUInfo.GetCPUFrequency);
+  UsageSeries.AddXY(FTimePoint, FCPUInfo.GetCPUUsage);
+  
+  if FTimePoint > 60 then
+  begin
+    FreqSeries.Delete(0);
+    UsageSeries.Delete(0);
+    Chart.BottomAxis.Minimum := FTimePoint - 60;
+    Chart.BottomAxis.Maximum := FTimePoint;
+  end;
 end;
 
 end.
---
+++ ucpuinfo.pas
@@ -0,0 +1,91 @@
+unit ucpuinfo;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Process;
+
+type
+  TCPUInfo = class
+  private
+    FPrevIdleTime: Int64;
+    FPrevTotalTime: Int64;
+    FCPUFrequency: Double;
+    FCPUUsage: Double;
+  public
+    constructor Create;
+    function GetCPUFrequency: Double;
+    function GetCPUUsage: Double;
+    property CPUFrequency: Double read FCPUFrequency;
+    property CPUUsage: Double read FCPUUsage;
+  end;
+
+implementation
+
+constructor TCPUInfo.Create;
+begin
+  inherited;
+  FPrevIdleTime := 0;
+  FPrevTotalTime := 0;
+  FCPUFrequency := 0;
+  FCPUUsage := 0;
+end;
+
+function TCPUInfo.GetCPUFrequency: Double;
+var
+  ProcessOutput: TStringList;
+begin
+  ProcessOutput := TStringList.Create;
+  try
+    ProcessOutput.LoadFromFile('/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq');
+    if ProcessOutput.Count > 0 then
+      FCPUFrequency := StrToFloat(ProcessOutput[0]) / 1000; // KHz to MHz
+  except
+    FCPUFrequency := 0;
+  end;
+  ProcessOutput.Free;
+  Result := FCPUFrequency;
+end;
+
+function TCPUInfo.GetCPUUsage: Double;
+var
+  ProcStat: TStringList;
+  Values: TStringList;
+  User, Nice, System, Idle: Int64;
+  Total, DiffIdle, DiffTotal: Int64;
+begin
+  Result := 0;
+  ProcStat := TStringList.Create;
+  Values := TStringList.Create;
+  try
+    ProcStat.LoadFromFile('/proc/stat');
+    if ProcStat.Count > 0 then
+    begin
+      Values.Delimiter := ' ';
+      Values.DelimitedText := ProcStat[0];
+      
+      User := StrToInt64(Values[1]);
+      Nice := StrToInt64(Values[2]);
+      System := StrToInt64(Values[3]);
+      Idle := StrToInt64(Values[4]);
+      
+      Total := User + Nice + System + Idle;
+      DiffIdle := Idle - FPrevIdleTime;
+      DiffTotal := Total - FPrevTotalTime;
+      
+      if DiffTotal > 0 then
+        FCPUUsage := 100.0 * (1.0 - DiffIdle / DiffTotal);
+        
+      FPrevIdleTime := Idle;
+      FPrevTotalTime := Total;
+      Result := FCPUUsage;
+    end;
+  finally
+    ProcStat.Free;
+    Values.Free;
+  end;
+end;
+
+end.
