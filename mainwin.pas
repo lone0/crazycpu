fcd5a3aadc068a487f0121d4d3db7b0aab3a53b7
Zhen XIN <lone.2004@gmail.com>
2025-01-29 02:29:53 +0800

    Charts for individual cores

--- mainwin.pas
+++ mainwin.pas
@@ -6,10 +6,16 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, ComCtrls,
-  StdCtrls, TAGraph, TASeries, TATransformations, ucpuinfo;  // Add Math unit here
+  StdCtrls, TAGraph, TASeries, TATransformations, TAChartAxisUtils, ucpuinfo;  // Add Math unit here
 
 type
   { TMainWindow }
+
+  TCoreChart = record
+    Chart: TChart;
+    FreqSeries: TLineSeries;
+    UsageSeries: TBarSeries;
+  end;
 
   TMainWindow = class(TForm)
     OverallChart: TChart;
@@ -31,8 +37,16 @@ type
   private
     FCPUManager: TCPUInfoManager;
     FTimePoint: Integer;
+    FCoreCharts: array of TCoreChart;
     const
-      MinsToShow = 60;  // Show 60 seconds of data
+      MinsToShow = 60;
+      CHARTS_PER_ROW = 2;
+      CHART_WIDTH = 400;
+      CHART_HEIGHT = 200;
+      CHART_MARGIN = 10;
+    procedure CreateCoreCharts;
+    procedure ConfigureChart(AChart: TChart; ATitle: string);
+    procedure UpdateCoreChart(ACoreChart: TCoreChart; ACPUInfo: TCPUInfo);
   public
 
   end;
@@ -64,8 +78,8 @@ begin
   OverallChart.AxisList[1].Range.Max := MinsToShow;
 
   // Configure axis ranges based on CPU capabilities
-  OverallChart.AxisList[0].Range.Min := FCPUManager.MinMHz;
-  OverallChart.AxisList[0].Range.Max := FCPUManager.MaxMHz;
+  OverallChart.AxisList[0].Range.Min := FCPUManager.MinMHz * 0.75;
+  OverallChart.AxisList[0].Range.Max := FCPUManager.MaxMHz + FCPUManager.MinMHz / 4;
   OverallChart.AxisList[0].Range.UseMin := True;
   OverallChart.AxisList[0].Range.UseMax := True;
 
@@ -73,6 +87,8 @@ begin
   UpdateTimer.Enabled := True;
 
   MainStatusBar.SimpleText := Format('Total Core: %d', [FCPUManager.CoreCount]);
+
+  CreateCoreCharts;
 end;
 
 procedure TMainWindow.FormDestroy(Sender: TObject);
@@ -139,6 +155,141 @@ begin
     UsageSeries.Delete(0);
     OverallChart.AxisList[1].Range.Min := FTimePoint - MinsToShow;
     OverallChart.AxisList[1].Range.Max := FTimePoint;
+  end;
+
+  // Update individual core charts
+  for i := 0 to High(FCoreCharts) do
+    UpdateCoreChart(FCoreCharts[i], FCPUManager.Cores[i]);
+end;
+
+procedure TMainWindow.CreateCoreCharts;
+var
+  i, row, col: Integer;
+  NewChart: TChart;
+  ScrollBox: TScrollBox;
+  TotalHeight: Integer;
+begin
+  SetLength(FCoreCharts, FCPUManager.CoreCount);
+  
+  // Create scrollbox in groupbox
+  ScrollBox := TScrollBox.Create(GroupBox1);
+  ScrollBox.Parent := GroupBox1;
+  ScrollBox.Align := alClient;
+  ScrollBox.HorzScrollBar.Visible := False;
+  
+  // Calculate total height needed
+  TotalHeight := ((FCPUManager.CoreCount + CHARTS_PER_ROW - 1) div CHARTS_PER_ROW) 
+                 * (CHART_HEIGHT + CHART_MARGIN);
+  
+  for i := 0 to FCPUManager.CoreCount - 1 do
+  begin
+    row := i div CHARTS_PER_ROW;
+    col := i mod CHARTS_PER_ROW;
+    
+    // Create chart
+    NewChart := TChart.Create(ScrollBox);
+    with NewChart do
+    begin
+      Parent := ScrollBox;
+      Left := col * (CHART_WIDTH + CHART_MARGIN);
+      Top := row * (CHART_HEIGHT + CHART_MARGIN);
+      Width := CHART_WIDTH;
+      Height := CHART_HEIGHT;
+    end;
+    
+    // Configure chart
+    ConfigureChart(NewChart, Format('Core %d', [FCPUManager.Cores[i].CPUNumber]));
+    
+    // Create series
+    with FCoreCharts[i] do
+    begin
+      Chart := NewChart;
+      
+      FreqSeries := TLineSeries.Create(Chart);
+      with FreqSeries do
+      begin
+        Chart.AddSeries(FreqSeries);
+        AxisIndexY := 0;
+        SeriesColor := clBlue;
+        Title := 'Frequency';
+      end;
+      
+      UsageSeries := TBarSeries.Create(Chart);
+      with UsageSeries do
+      begin
+        Chart.AddSeries(UsageSeries);
+        AxisIndexY := 2;
+        SeriesColor := clSilver;
+        Title := 'Usage';
+      end;
+    end;
+  end;
+end;
+
+procedure TMainWindow.ConfigureChart(AChart: TChart; ATitle: string);
+var
+  LeftTrans, RightTrans: TChartAxisTransformations;
+begin
+  with AChart do
+  begin
+    Title.Text.Text := ATitle;
+    Hint := ATitle;
+    ShowHint := true;
+    
+    // Create transformations
+    LeftTrans := TChartAxisTransformations.Create(self);
+    RightTrans := TChartAxisTransformations.Create(self);
+    
+    // Add auto-scale transforms
+    TAutoScaleAxisTransform.Create(LeftTrans).Transformations := LeftTrans;
+    TAutoScaleAxisTransform.Create(RightTrans).Transformations := RightTrans;
+    
+    // Left axis (Frequency)
+    //AxisList.Add.Alignment := calLeft;
+    with AxisList[0] do
+    begin
+      Alignment := calLeft;
+      Title.Caption := 'MHz';
+      Range.Min := FCPUManager.MinMHz;
+      Range.Max := FCPUManager.MaxMHz;
+      Range.UseMin := True;
+      Range.UseMax := True;
+      Transformations := LeftTrans;
+    end;
+    
+    // Bottom axis (Time)
+    //AxisList.Add.Alignment := calBottom;
+    AxisList[1].Title.Caption := 'Time';
+    
+    // Right axis (Usage)
+    AxisList.Add.Alignment := calRight;
+    with AxisList[2] do
+    begin
+      Alignment := calRight;
+      Title.Caption := '%';
+      Range.Min := 0;
+      Range.Max := 100;
+      Range.UseMin := True;
+      Range.UseMax := True;
+      Transformations := RightTrans;
+    end;
+  end;
+end;
+
+procedure TMainWindow.UpdateCoreChart(ACoreChart: TCoreChart; ACPUInfo: TCPUInfo);
+begin
+  with ACoreChart do
+  begin
+    FreqSeries.AddXY(FTimePoint, ACPUInfo.GetCPUFrequency);
+    UsageSeries.AddXY(FTimePoint, ACPUInfo.GetCPUUsage);
+    
+    if FTimePoint > MinsToShow then
+    begin
+      FreqSeries.Delete(0);
+      UsageSeries.Delete(0);
+      Chart.AxisList[1].Range.Min := FTimePoint - MinsToShow;
+      Chart.AxisList[1].Range.Max := FTimePoint;
+    end;
   end;
 end;
 
